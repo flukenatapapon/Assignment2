9dbfe0d309e76601afbf62d07f5e5daca00324ea
natapapon <natapapon.r@gmail.com>
2021-01-26 11:20:26 +0700

    Add template

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,17 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def shop
+    
+  end
+
+  def contact
+    
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  root 'welcome#index'
+
+  get 'index', to: 'welcome#index', as: 'index'
+  get 'about', to: 'welcome#about', as: 'about'
+  get 'shop', to: 'welcome#shop', as: 'shop'
+  get 'contact', to: 'welcome#contact', as: 'contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
